1f392f013460ce605ecc50e7b10528dd30c6e372
Marcelo S. Gomes <120570838+M4rc3low@users.noreply.github.com>
2026-09-07 16:43:12 -0300

    feat: add machinery and market domain models

---
+++ src/backend/AgroControl.Domain/Modules/Machinery/MachineKind.cs
@@ -0,0 +1,13 @@
+namespace AgroControl.Domain.Modules.Machinery;
+
+public enum MachineKind
+{
+    Tractor,
+    Harvester,
+    Sprayer,
+    Seeder,
+    Implement,
+    Truck,
+    UtilityVehicle,
+    Other
+}
